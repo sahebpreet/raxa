d9478739078f9daa28bc4c0fde58b6191d206508
Saheb Preet Singh <saheb.preet@gmail.com>
2014-03-19 15:24:32 +0530

    included punjabi language in localization

---
+++ app/locale/Messages_pa.js
@@ -0,0 +1,13 @@
+Messages.raxa.view.test.getLocationsButton.text = "ਇਲਾਜ ਟਿਕਾਣੇ ਲਵੋ";
+Messages.raxa.view.test.getProvidersButton.text = "ਡਾਕਟਰ 'ਟਿਕਾਣੇ ਲਵੋ";
+
+Messages.raxa.view.Locations.itemTpl = '<div>{display} (10.34 ਕਿ.ਮੀ.)</div><div>★★★☆☆</div>';
+Messages.raxa.view.Locations.emptyText =  'ਵੇਖਾਉਣ ਲਈ ਕੋਈ ਡਾਟਾ ਨਹੀ';
+Messages.raxa.view.Locations.title = "ਆਪ ਜੀ ਦੇ ਨੇੜੇ ਇਲਾਜ ਟਿਕਾਣੇ";
+
+Messages.raxa.view.Providers.itemTpl = '<div>{display} (10.34 ਕਿ.ਮੀ.)</div><div>★★★☆☆</div>';
+Messages.raxa.view.Providers.emptyText =  'ਵੇਖਾਉਣ ਲਈ ਕੋਈ ਡਾਟਾ ਨਹੀ';
+Messages.raxa.view.Providers.title = "ਤੁਹਾਡੇ ਨੇੜੇ ਡਾਕਟਰ";
+
+Messages.raxa.view.Main.defaultBackButtonText = "ਵਾਪਸ";
+Messages.raxa.view.Main.title = "ਰੈਕ੍ਸਾ  ਡੈਮੋ ਅਨੁਪ੍ਰਯੋਗ";
